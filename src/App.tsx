e8acde43c70efbfcca3b7111a3917df92aa54da4
TheHunterIA <cleitongprado@gmail.com>
2026-04-01 16:45:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import ScrollToTop from './components/layout/ScrollToTop';
-import ForceRedirectOnReload from './components/layout/ForceRedirectOnReload';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -19,7 +18,6 @@ export default function App() {
   return (
     <Router>
       <ScrollToTop />
-      <ForceRedirectOnReload />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
